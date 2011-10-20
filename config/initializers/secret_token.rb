345c597b95ddacb47fa9746685ca474a1ea6f040
Ed Richard <ed@bob-mbp2.home>
2011-10-19 20:34:45 -0400

    iniital commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Articles::Application.config.secret_token = '739b272b9330c31d66bc756165a01c7f20700be78bc22253b1bd92911b84b6f957b4985fcc2d469d95f16a0e79968120fb4dbf56891c1ae9741cd338dbe1ab4e'
---
+++ test/unit/article_test.rb
@@ -0,0 +1,21 @@
+require 'test_helper'
+
+class ArticleTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+  test "should create article" do
+      article = Article.new
+      
+      article.user = users(:eugene)
+      article.title = "Test article"
+      article.body = "Test body"
+      
+      assert article.save
+  end
+  
+  test "should find article" do
+    article_id = articles(:welcome_to_rails).id
+    assert_nothing_raised { Article.find(article_id)}
+  end
+end
